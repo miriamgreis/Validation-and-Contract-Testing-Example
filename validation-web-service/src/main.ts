68801e5985d759f842a0bbe0222470975345b210
Miriam Greis <miriam.greis@codecentric.de>
2024-02-27 08:36:54 +0100

    Add further topics regarding contract testing

---
+++ validation-web-service/src/main.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { MulterModule } from '@nestjs/platform-express';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {});
+
+  const config = new DocumentBuilder()
+    .setTitle('validation web service')
+    .setDescription(
+      'The API specification of the API validation service. No additional authorization is required using this UI. ' +
+        'This is a showcase utilizing Spectral and a custom ruleset.',
+    )
+    .setVersion('0.1')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  MulterModule.register({
+    dest: './upload',
+  });
+
+  await app.listen(3000);
+}
+bootstrap();
